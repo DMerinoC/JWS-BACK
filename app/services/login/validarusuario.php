ea916195a2d5ab183e532e8f0b554eba2cb219d2
DiegoMC <damc99@hotmail.com>
2024-10-30 22:33:51 -0500

    verificacion

--- app/services/login/validarusuario.php
+++ app/services/login/validarusuario.php
@@ -1,13 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:8080");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200); // OK
-    exit();
-}
-
 require_once '../../models/trabajador.php';
 require_once '../../config/tokenJWT.php';
 
